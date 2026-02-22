9693f8ee6a77353e63b33e46b65b9bb278c3655b
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-02-22 13:25:09 +0530

    Added Advanced REST API

--- src/main/java/com/in28minutes/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/helloworld/HelloWorldController.java
@@ -23,4 +23,9 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return "Hello World v2";
+    }
 }
--- src/test/java/com/in28minutes/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservuces.restful_web_services;
+package com.in28minutes.rest.webservices.restful_web_services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
